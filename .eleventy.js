1ac1927c2ad0bf00898eeb40777ea6d9fcc37bd7
Albert Volkman <albert.volkman@gmail.com>
2026-03-24 15:16:41 -0400

    Updates

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,17 @@
 export default function (eleventyConfig) {
-  eleventyConfig.ignores.add('_rendered');
-  eleventyConfig.ignores.add('_cleaned');
+  eleventyConfig.ignores.add("_rendered");
+  eleventyConfig.ignores.add("_cleaned");
+  eleventyConfig.ignores.add("playwright-report");
+  eleventyConfig.ignores.add("test-results");
+  eleventyConfig.ignores.add("tests");
+  eleventyConfig.ignores.add("node_modules");
   eleventyConfig.addPassthroughCopy({
-    '__fonts': '__fonts',
-    '__static': '__static',
-    'bundle': 'bundle',
-    'favicons': 'favicons',
-    'styles': 'styles',
+    __fonts: "__fonts",
+    __static: "__static",
+    favicons: "favicons",
+    styles: "styles",
   });
-  eleventyConfig.addGlobalData('site_title', 'Hancock Plumbing Co. Inc.');
-  eleventyConfig.setIncludesDirectory('_includes');
-  eleventyConfig.setLayoutsDirectory('_layouts');
-};
+  eleventyConfig.addGlobalData("site_title", "Hancock Plumbing Co. Inc.");
+  eleventyConfig.setIncludesDirectory("_includes");
+  eleventyConfig.setLayoutsDirectory("_layouts");
+}
